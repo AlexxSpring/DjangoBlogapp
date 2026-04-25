69114c202d64765d9fe95a42564a17b27585a9d9
Springx <264676501+AlexxSpring@users.noreply.github.com>
2026-04-26 01:09:49 +0530

    fixes and changes

--- blogapplication/settings.py
+++ blogapplication/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Added manually
 # When the static folder is in the root directory
