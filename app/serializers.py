0821df8118ac55e152257d26832a183b5da11d5c
Springx <264676501+AlexxSpring@users.noreply.github.com>
2026-04-25 20:48:34 +0530

    Added serializers

--- app/serializers.py
+++ app/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
-    # show username instead of user id
     author = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
