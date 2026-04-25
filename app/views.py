d73568ead2652dc35fb2f0fcc66aed91cd1cf107
Springx <264676501+AlexxSpring@users.noreply.github.com>
2026-04-25 22:48:53 +0530

    basic layout

--- app/views.py
+++ app/views.py
@@ -3,6 +3,12 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
+# Frontend Imports
+from django.shortcuts import render
+from .models import Post
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+
 from .models import Post
 from .serializers import PostSerializer
 from .permissions import IsAuthorOrReadOnly
@@ -17,5 +23,29 @@ class PostViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+######################################################
 
+# FrontEnd
+
+
+
+def home_page(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'posts': posts})
+
+
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+
+    return render(request, 'login.html')
 
--- blogapplication/urls.py
+++ blogapplication/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from app.views import home_page, login_page
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -8,6 +9,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page, name='home'),
+    path('login/', login_page, name='login'),
 
     # Blog API
     path('api/', include('app.urls')),
@@ -16,3 +19,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+
